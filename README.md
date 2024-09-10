66e4407521b2c7dc020176977a7b3c872a38e9b9
enkyuan <yuan_enkang@outlook.com>
2024-09-10 18:23:48 -0500

    commented main.java & modified README.md

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 ## Project Description
 
+
 ## Java Version
 
+```shell
+openjdk version "22.0.2" 2024-07-16
+```
+
 ## Sample Output
+
--- main.java
+++ main.java
@@ -1,13 +1,15 @@
 // TODO: reformat the output to display the prime numbers in a single line 
-// TODO: document the code (almost every line)
 // TODO: modify the README to include description, Java version, and sample output
-
+// TODO: check if line 41 is valid according to the grader's specifications
+    
 /*
- *
- *
+ * Name:    Enkang Yuan
+ * Sect.:   CS 3345.503
+ * Desc.:   This program takes an integer N > 1 and prints all prime numbers from 2 to N.
  *
  */
 
+// import statements
 import static java.lang.System.out;
 import java.util.Scanner;
 import java.util.Arrays;
@@ -19,15 +21,18 @@ public class main {
         
         out.print("Enter a positive integer greater than 1: ");
         int N = io.nextInt();
-
+        
+        // checks if input is correct (greater than 1); if not, user is prompted until it is valid
         while (!(N >= 1)) {
             out.print("Enter a positive integer greater 1: ");
             N = io.nextInt();
         }
 
+        // creates a boolean array of size N + 1 & fills it with true
         boolean[] A = new boolean[N + 1];
         Arrays.fill(A, true);
-
+        
+        // checks if numbers are prime or not using the sieve algorithm
         for(int i = 2; i <= Math.sqrt(N); i++) {
            if (A[i] == true) {
                for(int j = i * i; j <= N; j += i) {
@@ -36,6 +41,7 @@ public class main {
            }
         }
         
+        // output of the prime numbers from 2 to N using java intstream
         out.println();
         out.println("From 2 to " + N + " the prime numbers are " + String.join(", ", IntStream.range(2, A.length).filter(i -> A[i]).mapToObj(String::valueOf).toArray(String[]::new)));
     }
